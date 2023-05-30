f96732b54d20ae88245eac484eb0f95af8fb527c
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-05-30 10:31:40 -0300

    Projeto web

---
+++ src/app/api/get_inventory/route.ts
@@ -0,0 +1,7 @@
+import { getInvetory } from "@/components/services/Invetory";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET (resquest: NextRequest) {
+  const item = await getInvetory()
+  return NextResponse.json( item )
+}
---
+++ src/components/services/Invetory.ts
@@ -0,0 +1,16 @@
+import { Api } from "../providers/Api";
+import { revalidateTag } from "next/cache";
+
+export async function getInvetory() {
+  try {
+    const response = await fetch(
+      "https://steamcommunity.com/inventory/76561198862407248/730/2?l=english&count=500%27%27%27"
+    );
+    const data = await response.json();
+    revalidateTag(data.descriptions);
+    console.log(data.descriptions);
+    return data.descriptions;
+  } catch (error) {
+    console.log(error);
+  }
+}
