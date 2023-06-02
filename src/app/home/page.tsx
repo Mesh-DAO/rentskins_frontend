2898a4e469348842a478f5a7bda65f3bba41ebf2
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-06-02 11:35:58 -0300

    feat(LayoutPage): Criando o component LayoutPage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,41 +1,38 @@
 import { SteamButton } from "@/components/SteamButton";
 import { HeroInformation } from "@/components/HeroInformation";
-import { Footer } from "@/components/Footer";
-
 import ShieldIcon from '../../components/icons/icon-shield.svg';
 import PhoneIcon from '../../components/icons/icon-phone.svg';
 import MagnifyingGlassIcon from '../../components/icons/icon-magnifying-glass.svg';
 import DevolutionIcon from '../../components/icons/icon-devolution.svg';
-import Header from "@/components/Shared/Header";
+import { LayoutPage } from "@/components/Shared";
 
-export default function Home(){
+export default function Home() {
   return (
-    <div className="bg-mesh-dark-0">
-      <Header />
-      <main className="h-[2000px]">
-        {/* Hero */}
-        <div className="flex flex-col items-center justify-center bg-cover bg-hero-image bg-no-repeat bg-center h-1/2">
+      <LayoutPage>
+        <main className="h-[2000px]">
+          {/* Hero */}
+          <div className="flex flex-col items-center justify-center bg-cover bg-hero-image bg-no-repeat bg-center h-1/2">
 
-          {/* Hero - Content */}
-          <div className="space-y-8 text-center flex flex-col items-center text-white">
-            <p className="max-w-2xl font-bold text-[3.5rem] leading-none">
-              <span> Descubra o mundo das skins{' '}
-                <strong className="bg-green-pattern text-transparent bg-clip-text">
-                  CS:GO
-                </strong>
-              </span>
-            </p>
-            <p className="max-w-3xl text-2xl">
-              Personalize seu arsenal com as skins mais incríveis, encontrando as
-              skins perfeitas para dominar o jogo!
-            </p>
-            <SteamButton />
-          </div>
+            {/* Hero - Content */}
+            <div className="space-y-8 text-center flex flex-col items-center text-white">
+              <p className="max-w-2xl font-bold text-[3.5rem] leading-none">
+                <span> Descubra o mundo das skins{' '}
+                  <strong className="bg-green-pattern text-transparent bg-clip-text">
+                    CS:GO
+                  </strong>
+                </span>
+              </p>
+              <p className="max-w-3xl text-2xl">
+                Personalize seu arsenal com as skins mais incríveis, encontrando as
+                skins perfeitas para dominar o jogo!
+              </p>
+              <SteamButton />
+            </div>
           </div>
           {/* Hero - Bottom */}
           <div className="bg-mesh-dark-1 w-full h-40">
             {/* Hero - Green Line */}
-            <hr className="w-full -mt-0.5 h-2 bg-green-pattern"/>
+            <hr className="w-full -mt-0.5 h-2 bg-green-pattern" />
 
             {/* Hero - Information */}
             <div className="flex items-center h-full justify-center">
@@ -57,9 +54,8 @@ export default function Home(){
                 Facilitamos a devolução ou troca, de acordo com nossos termos.
               </HeroInformation>
             </div>
-        </div>
-      </main>
-      <Footer />
-    </div>
+          </div>
+        </main>
+      </LayoutPage>
   )
 }
---
+++ src/components/Shared/LayoutPage.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Header from "./Header";
+import { Footer } from "../Footer";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function LayoutPage({ children }: Props) {
+  return (
+    <div className="min-h-screen bg-[#FAFAFA]">
+      <Header />
+        {children}
+      <Footer />
+    </div>
+  );
+}
