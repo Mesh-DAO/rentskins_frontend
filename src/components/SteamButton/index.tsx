13333ba4795db0468c1712724e46760f6961c4b3
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-06-05 11:43:23 -0300

    Resolvendo error de prettier 2

--- src/components/SteamButton/index.tsx
+++ src/components/SteamButton/index.tsx
@@ -1,29 +1,31 @@
-import React from "react";
-import { MouseEventHandler } from "react";
-import Image from "next/image";
-import SteamIcon from "../../components/icons/social/icon-steam.svg";
+import React, { MouseEventHandler } from 'react'
+import Image from 'next/image'
+import SteamIcon from '../../components/icons/social/icon-steam.svg'
 
 type Props = {
-  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
-  width?: string;
-  height?: string;
-  disable?: boolean;
-};
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined
+  width?: string
+  height?: string
+  disable?: boolean
+}
 
 export function SteamButton({
   onClick,
-  height = "h-[60px]",
-  width = "w-[330px]",
+  height = 'h-[60px]',
+  width = 'w-[330px]',
 }: Props) {
   return (
     <button
       onClick={onClick}
-      type='button'
-      className={`rounded-md font-Roboto transition-all ease-in-out duration-300 no-underline ${width} ${height} flex gap-4 justify-center items-center
+      type="button"
+      className={`font-Roboto rounded-md no-underline transition-all duration-300 ease-in-out ${width} ${height} flex items-center justify-center gap-4
       bg-alt-steam-button transition-colors hover:bg-steam-button-pattern`}
     >
-      <Image src={SteamIcon} alt="Steam Login" width={24} height={24}/>
-      <span className="text-[22px] text-mesh-dark-2 font-bold"> Entre com a sua Steam </span>
+      <Image src={SteamIcon} alt="Steam Login" width={24} height={24} />
+      <span className="text-[22px] font-bold text-mesh-dark-2">
+        {' '}
+        Entre com a sua Steam{' '}
+      </span>
     </button>
-  );
+  )
 }
