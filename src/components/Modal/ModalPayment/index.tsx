f30e60a592ecd4ddb82cce37ca1127ee47722800
Gabriel Almeida <viniciusgsa99@gmail.com>
2023-06-07 11:00:20 -0300

    Refactor: Retirando a feature de fachar o modal utilizandod o botão x (O Tiago já implementou.

--- src/components/Modal/ModalPayment/index.tsx
+++ src/components/Modal/ModalPayment/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { IconClose } from '@/components/Icons/IconClose'
 import { Title } from '@/components/Title'
