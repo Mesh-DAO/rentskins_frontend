18a571de1f3c86a240ea9cf0feb988d44fb9317d
ItaloAraujoDev <italoarauju2019@gmail.com>
2023-06-12 18:35:10 -0300

    Resolvendo os conflitos da vercel

--- src/stores/components.store.ts
+++ src/stores/components.store.ts
@@ -1,4 +1,5 @@
 import create from 'zustand'
+import { IPaymentRefound, PaymentMethodRefound } from './Interfaces'
 
 interface IStates {
   paymentGeneralIndex: 0 | 1 | 2
@@ -9,6 +10,12 @@ interface IStates {
   setPaymentWithdrawIndex: (index: 0 | 1 | 2 | 3) => void
   settingsIndex: 0 | 1 | 2
   setSettingsIndex: (index: 0 | 1 | 2) => void
+  profileTabValue: 'sales' | 'rented'
+  setProfileTabValue: (value: 'sales' | 'rented') => void
+  refoundGeneralIndex: 0 | 1 | 2 | 3
+  setRefoundGeneralIndex: (index: 0 | 1 | 2 | 3) => void
+  paymentRefound: { value: number; method: PaymentMethodRefound }
+  setPaymentRefound: (paymentRefound: IPaymentRefound) => void
 }
 
 const useComponentStore = create<IStates>((set) => ({
@@ -30,6 +37,21 @@ const useComponentStore = create<IStates>((set) => ({
   settingsIndex: 0,
   setSettingsIndex: (index: 0 | 1 | 2) => {
     set(() => ({ settingsIndex: index }))
+  },
+
+  refoundGeneralIndex: 0,
+  setRefoundGeneralIndex: (index: 0 | 1 | 2 | 3) => {
+    set(() => ({ refoundGeneralIndex: index }))
+  },
+
+  profileTabValue: 'sales',
+  setProfileTabValue: (value: 'sales' | 'rented') => {
+    set(() => ({ profileTabValue: value }))
+  },
+
+  paymentRefound: { value: 5, method: 'mastercard' },
+  setPaymentRefound: (paymentRefound: IPaymentRefound) => {
+    set(() => ({ paymentRefound }))
   },
 }))
 
