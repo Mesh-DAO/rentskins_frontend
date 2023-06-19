b1eb3caf29fd77150489da6f2b5eb4b8fb2518d7
ItaloAraujodev <italoarauju2019@gmail.com>
2023-06-19 14:04:46 -0300

    feat: Deixando dinamico a home e details

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -2,34 +2,42 @@
 import { Card, InfoPerfil, InfoVendas, InfoSkin } from '@/components/Details'
 import { LayoutPage } from '@/components/Shared/LayoutPage'
 import { findById } from '@/services/SkinService'
+import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'next/navigation'
-import { useEffect } from 'react'
 
 export default function Details() {
   const { id } = useParams()
 
-  useEffect((): any => {
-    const find = async () => {
-      const findId = await findById(id)
-      console.log(findId)
-    }
-
-    return find
-  }, [])
+  const { data, isLoading } = useQuery({
+    queryKey: ['skin'],
+    queryFn: () => findById(id),
+  })
 
   return (
     <LayoutPage>
       <main className="w-full bg-mesh-color-others-black">
-        <div className="mx-auto flex w-10/12 justify-center py-10">
-          <div>
-            <Card />
-            <InfoVendas />
+        {!isLoading && (
+          <div className="mx-auto flex w-10/12 justify-center py-10">
+            <div>
+              <Card
+                skinImage={data!.data.skin_image}
+                skinName={data!.data.skin_name}
+              />
+              <InfoVendas />
+            </div>
+            <div className="ml-4">
+              <InfoSkin
+                skinName={data!.data.skin_name}
+                price={data!.data.skin_price}
+                float={data!.data.skin_float}
+                type={data!.data.skin_category}
+                arma={data!.data.skin_weapon}
+                sellerId={data!.data.seller_id}
+              />
+              <InfoPerfil />
+            </div>
           </div>
-          <div className="ml-4">
-            <InfoSkin />
-            <InfoPerfil />
-          </div>
-        </div>
+        )}
       </main>
     </LayoutPage>
   )
