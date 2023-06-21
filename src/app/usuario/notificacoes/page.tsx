64b4f26ef16656d9425155f2c6cd2a46ac0f2a2a
ztiagok <ztiagok@gmail.com>
2023-06-21 14:32:35 -0300

    feat: Adição dos input radios

--- src/app/usuario/notificacoes/page.tsx
+++ src/app/usuario/notificacoes/page.tsx
@@ -3,29 +3,26 @@ import { Button } from '@/components/Button'
 import { LayoutPage } from '@/components/Shared'
 import { Title } from '@/components/Title'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 export default function NotificationPage() {
-  const [title, setTitle] = useState('')
-
   const searchParams = useSearchParams()
   const router = useRouter()
   const pathname = usePathname()
 
   useEffect(() => {
-    const titleQuery = searchParams.get('notification') as
-      | 'historic'
-      | 'transactions'
+    const titleQuery = searchParams.get('type') as 'historic' | 'transactions'
 
     if (titleQuery !== 'historic') {
       if (titleQuery !== 'transactions') {
-        router.push()
+        router.push(pathname + '/?type=historic')
       }
     }
   }, [])
 
-  const handleOnRadio = (event) => {
-    router.push(pathname + '/?notification=' + event.target.value)
+  // MUDAR TIPAGEM
+  const handleOnRadio = ({ target }: any) => {
+    router.push(pathname + '/?type=' + target.value)
   }
 
   return (
@@ -36,29 +33,38 @@ export default function NotificationPage() {
         </Title>
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-6">
-            <label>
-              <span className="text-xl font-bold text-white/50">
-                Transações
-              </span>
+            <label className="flex cursor-pointer flex-col">
               <input
                 type="radio"
                 name="notification-radio"
-                defaultChecked={true}
+                className="peer appearance-none"
+                defaultChecked={searchParams.get('type') === 'transactions'}
                 value={'transactions'}
                 onClick={(event) => handleOnRadio(event)}
               />
+              <span className="text-xl font-semibold text-white/50 transition-all peer-checked:text-white">
+                Transações
+              </span>
+              <div className="mt-2 h-0.5 w-0 place-self-center bg-mesh-color-primary-900 pl-0 transition-all peer-checked:pl-28" />
             </label>
-            <label>
-              <span className="text-xl font-bold text-white">Histórico</span>
+            <label className="flex cursor-pointer flex-col">
               <input
                 type="radio"
                 name="notification-radio"
+                className="peer appearance-none"
+                defaultChecked={searchParams.get('type') === 'historic'}
                 value={'historic'}
                 onClick={(event) => handleOnRadio(event)}
               />
+              <span className="text-xl font-semibold text-white/50 transition-all peer-checked:text-white">
+                Histórico
+              </span>
+              <div className="mt-2 h-0.5 w-0 place-self-center bg-mesh-color-primary-900 pl-0 transition-all peer-checked:pl-20" />
             </label>
           </div>
-          <Button>Tudo</Button>
+          <Button className="border-none bg-mesh-color-primary-1200 px-3 py-1 font-semibold">
+            Tudo
+          </Button>
         </div>
       </main>
     </LayoutPage>
