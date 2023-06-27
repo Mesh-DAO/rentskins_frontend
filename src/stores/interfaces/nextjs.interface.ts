46b948f390256c553f7b7f21456272733a39bf4c
ztiagok <ztiagok@gmail.com>
2023-06-27 16:33:53 -0300

    refactor: Removendo NextJS store criado no commit passado

--- src/stores/interfaces/nextjs.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IMetadata {
-  title: string
-  description: string
-}
-
-export interface IStates {
-  customMetadata: IMetadata
-  setCustomMetadata: (customMetadata: IMetadata) => void
-}
