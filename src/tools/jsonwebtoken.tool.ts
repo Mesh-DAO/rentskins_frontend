e73b500f240c0b10d6b43b0aa48fc6a7d5553c3a
ztiagok <ztiagok@gmail.com>
2023-06-29 14:33:18 -0300

    feat: Adicionando variáveis de ambientes ao servidor interno e à tool JWT

--- src/tools/jsonwebtoken.tool.ts
+++ src/tools/jsonwebtoken.tool.ts
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken'
 
 export default class JsonWebToken {
   public static create(item: object) {
-    return jwt.sign(item, 'secret')
+    return jwt.sign(item, process.env.NEXT_PUBLIC_SECRET_KEY as string)
   }
 
   public static verify(token: string) {
-    const data = jwt.verify(token, 'secret')
+    const data = jwt.verify(token, process.env.NEXT_PUBLIC_SECRET_KEY as string)
 
     return data
   }
