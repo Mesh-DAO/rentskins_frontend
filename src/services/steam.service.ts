122918aca4310d07a07d2a2e272e34c3b203e6ad
ztiagok <ztiagok@gmail.com>
2023-06-30 17:39:28 -0300

    fix: Removendo comentário

--- src/services/steam.service.ts
+++ src/services/steam.service.ts
@@ -2,7 +2,6 @@ export default class SteamService {
   public static redirect() {
     window.location.replace(
       `${process.env.NEXT_PUBLIC_URL}/api/auth/steam/authenticate/`,
-      // 'http://localhost:3001/auth/steam/authenticate',
     )
   }
 }
