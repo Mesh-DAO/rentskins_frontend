a293750523873ef33691306a7a1b40f92feaac4a
ztiagok <ztiagok@gmail.com>
2023-07-03 18:38:30 -0300

    feat: Adicionando tempo passado nas notificações

--- src/components/NotificationCard/NotificationTime.tsx
+++ src/components/NotificationCard/NotificationTime.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 interface IProps {
-  timestamp: number | string
+  timestamp: number
 }
 
 export function NotificationTime({ timestamp }: IProps) {
+  const handleTimestamp = () => {
+    if (timestamp < 10) {
+      return 'Há alguns segundos'
+    } else if (timestamp < 60) {
+      return `Há ${timestamp} segundos`
+    } else if (timestamp < 3600) {
+      const elapsedTime = Math.floor(timestamp / 60)
+      return `Há ${elapsedTime} minutos`
+    } else {
+      const elapsedTime = Math.floor(timestamp / 3600)
+      return `Há ${elapsedTime} horas`
+    }
+  }
   return (
-    <span className="p-3 text-mesh-color-neutral-400">{timestamp} min</span>
+    <span className="p-3 text-mesh-color-neutral-400">{handleTimestamp()}</span>
   )
 }
